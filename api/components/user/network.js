c44e3355069a530c7205d81617af4611501ca36f
Abdiel Carreño <56939611+zayelmech@users.noreply.github.com>
2021-04-24 02:26:21 -0500

    Aislar el codigo de la base de datos clase 4

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const response = require('../../../network/response')
-
+const response = require('../../../network/response');
+const controller = require('./controller');
 
 const router = express.Router();
 
 
 
 router.get('/', function(req, res) {
-    response.success(req, res, 'Todo funciona', 200);
+    const lista = controller.list();
+    response.success(req, res, lista, 200);
 });
 
 module.exports = router;
